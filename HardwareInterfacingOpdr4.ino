1668cf38321d0af415cdfc81e45f1cbf8ddadb90
KingWazzack <61501251+KingWazzack@users.noreply.github.com>
2023-04-18 16:03:49 +0200

    Update HardwareInterfacingOpdr4.ino

--- HardwareInterfacingOpdr4.ino
+++ HardwareInterfacingOpdr4.ino
@@ -1,14 +1,18 @@
+//Door middel van een for loop zetten we de modus van. pin 7 t/m pin 13 op OUTPUT
 void setup(){
   for(int initializepin = 7; initializepin <= 13; initializepin++){
     pinMode(initializepin, OUTPUT);
   }
+  //We starten de serial connection met baudrate 9600
   Serial.begin(9600);
 }
 
 void loop(){
+  //Allereerst halen we de invoer op uit pin A0 waar de potentiometer aan hangt.
   int meting = analogRead(A0);
   Serial.println(meting);
 
+  //Vervolgens kijken we aan de hand hiervan welke pins aan en uit moeten en geven dit door aan de desbetreffende functies
   if(meting >= 0 && meting < 128){
     uitzetten(7,13);
   }
@@ -35,13 +39,14 @@ void loop(){
     }
   }
 
+//Deze functie zet door middel van een FOR loop alle pins op LOW die tussen de twee pins zitten die als paramater worden ontvangen.
 void uitzetten(int laagstepin, int hoogstepin){
   for(int pin = laagstepin; pin <= hoogstepin; pin++){
     digitalWrite(pin, LOW);
     }
 }
 
-
+//Deze functie zet door middel van een FOR loop alle pins op HIGH die tussen de twee pins zitten die als paramater worden ontvangen.
 void aanzetten(int laagstepin, int hoogstepin){
   for(int pin = laagstepin; pin <= hoogstepin; pin++){
     digitalWrite(pin, HIGH);
