73dbd08da21edefa4e6ad1096482b122e14330cc
KingWazzack <61501251+KingWazzack@users.noreply.github.com>
2023-04-18 15:53:42 +0200

    Update HardwareInterfacingOpdr1.ino

--- HardwareInterfacingOpdr1.ino
+++ HardwareInterfacingOpdr1.ino
@@ -1,8 +1,9 @@
+//we stellen de modus in van de pins die we gebruiken voor de twee leds
 void setup() {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
 }
-
+//we schakelen de ene lamp aan en de andere uit, wachten 1seconde en doen daarna het tegenovergestelde
 void loop() {
 digitalWrite(11, HIGH);
 digitalWrite(12,LOW);
